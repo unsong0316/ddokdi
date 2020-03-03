787eb3de1fafa25501fcd3f1c586709f9e454c7f
unsong0316 <uschoe1994@gmail.com>
2020-03-03 17:19:55 +0900

    almost

--- routes/eventboard_regi.js
+++ routes/eventboard_regi.js
@@ -32,59 +32,63 @@ exports.event_register= function(req,res){
               "timestamp": moment(Date.now()).format('YYYY-MM-DD HH:mm:ss'),
               "event_admin":req.body.payload.event_admin
             }
-            connection.query('INSERT INTO event SET ?',event_register, function (error, results, fields) {
+            connection.query('INSERT INTO event SET ?',event_register, 
+              function (error, results) {
               if (error) {
                   console.log("error ocurred",error);
                 res.send({
                     "code":400,
-                    "failed":"error ocurred"
+                    "faileds":"error ocurred"
                     });
               }
               else{
                 var default_value = 0
-                var user_event_event_no = connection.query(`SELECT MAX(event_no) FROM event`);
-
-                // 유저 수 가져오기
-                var user_count = connection.query('SELECT COUNT(*) FROM user WHERE admin = 0');
-
-                // 유저 수 만큼 user_event에 값 넣기
-                // var i = 0
-                for(var i=1; i<user_count; i++){
-                  //해당 유저 ID 가져오기
-                  var USERID = connection.query('SELECT USERID FROM user WHERE admin = 0');
-                  
-                  var create_user_event_update={
-                        "user_event_USERID": USERID,
-                        "checking": default_value,
-                        "participation":default_value,
-                        "user_event_event_no": user_event_event_no
-                      }                
-                  // user_event에 값 넣기
-                  connection.query('INSERT INTO user_event SET ?', create_user_event_update, function (error, results, fields) {
-                    if (error) {
-                        console.log("error ocurred",error);
-                      res.send({
-                          "code":400,
-                          "failed":"error ocurred"
-                          }
-                          );
-                        }
-                    else {
-                      a ="create_user_event_update done!"
-                      res.send({
-                        a
-                      });
-                    }
-                  console.log('The solution is: ', results);
-                  res.send({
-                      "code":200,
-                      "success":"event registered sucessfully"
+                var user_event_event_no = connection.query(`SELECT MAX(event_no) as EVENT_NO FROM event`, 
+                  function (error, EVENT_NO) {
+                    if(error) {return console.error(error);}
+                                   
+                    // console.log(EVENT_NO[0].EVENT_NO);
+                    // var user_count = connection.query('SELECT COUNT (USERID) as USER_NO FROM user WHERE admin = 0',  //유저 수 가져오기
+                    //   function(error,results){  
+                    //     if(error){return console.error(error);}
+                    //     USER_NO = results
+                    //     console.log(USER_NO);
+                    //     for(var i=0; i<USER_NO; i++){       //유저 수 만큼 user_event에 값넣기
+                    //       var USERID = connection.query('SELECT USERID FROM user as USERID WHERE admin = 0',  //해당 유저 ID 가져오기
+                    //       function(error,reuslts){   
+                    //         if(error){return console.error(error);}
+                    //         USERID = results
+                    //         console.log(USERID);
+                            var create_user_event_update={
+                              //"user_event_USERID": USERID,
+                              "checking": default_value,
+                              "participation":default_value,
+                              "user_event_event_no": EVENT_NO[0].EVENT_NO
+                            }
+                            connection.query('INSERT INTO user_event(user_event_USERID) SELECT USERID as user_event_USERID FROM user WHERE admin=0', 
+                            function (error, results) {
+                              if(error){return console.error(error);}
+                              console.log(results)
+                              // a = results
+                              // res.send({a});
+                              connection.query('UPDATE user_event SET ? where checking = NULL and participation = NULL and user_event_event_no = NULL ', create_user_event_update,
+                              function(error,results){
+                                if(error){return console.error(error);}
+                                console.log(results)
+                                
+                              });
                             });
-                      });
-                  }
-                }
-           });
-          }
+                          });
+                        }  
+
+                  // }
+                  // );
+                // }
+                // );
+          // }
+        }
+        );
+        }
           else{
             console.log("error ocurred",error);
             res.send({
@@ -95,6 +99,96 @@ exports.event_register= function(req,res){
         }
         );
       }
+
+////////////////////////////
+// var user_count = connection.query('SELECT COUNT (USERID) as USER_NO FROM user WHERE admin = 0',  //유저 수 가져오기
+//                       function(error,results){  
+//                         if(error){return console.error(error);}
+//                         USER_NO = results
+//                         console.log(USER_NO);
+//                         for(var i=0; i<USER_NO; i++){       //유저 수 만큼 user_event에 값넣기
+//                           var USERID = connection.query('SELECT USERID FROM user as USERID WHERE admin = 0',  //해당 유저 ID 가져오기
+//                           function(error,reuslts){   
+//                             if(error){return console.error(error);}
+//                             USERID = results
+//                             console.log(USERID);
+//                             var create_user_event_update={
+//                               "user_event_USERID": USERID,
+//                               "checking": default_value,
+//                               "participation":default_value,
+//                               "user_event_event_no": USER_NO
+//                             }
+//                             connection.query('INSERT INTO user_event SET ?', create_user_event_update, function (error, results) {
+//                               if(error){return console.error(error);}
+//                               a ="create_user_event_update done!"
+//                               res.send({a});
+//                             });
+//                           });
+//                         }  
+
+//                   });
+//                 });
+//           }
+//         });
+//         }
+//           else{
+//             console.log("error ocurred",error);
+//             res.send({
+//               "code":201,
+//               "success":"not available"
+//             });
+//           }
+//         }
+//         );
+//       }
+
+      /////////////////////
+
+ // 유저 수 가져오기
+                
+
+                // 유저 수 만큼 user_event에 값 넣기
+                // var i = 0
+          //       for(var i=0; i<user_count; i++){
+          //         //해당 유저 ID 가져오기
+          //         var USERID = connection.query('SELECT USERID FROM user WHERE admin = 0');
+                  
+          //         var create_user_event_update={
+          //               "user_event_USERID": USERID,
+          //               "checking": default_value,
+          //               "participation":default_value,
+          //               "user_event_event_no": user_event_event_no
+          //             }                
+          //         // user_event에 값 넣기
+          //         connection.query('INSERT INTO user_event SET ?', create_user_event_update, function (error, results, fields) {
+          //           if (error) {
+          //               console.log("error ocurred",error);
+          //             res.send({
+          //                 "code":400,
+          //                 "failed":"error ocurred"
+          //                 }
+          //                 );
+          //               }
+          //           else {
+          //             a ="create_user_event_update done!"
+          //             res.send({
+          //               a
+          //             });
+          //           }
+          //         console.log('The solution is: ', results);
+          //         res.send({
+          //             "code":200,
+          //             "success":"event registered sucessfully"
+          //                   });
+          //             });
+          //         }
+          //       }
+          //  });
+
+
+
+
+
       //           var default_value = "0"
       //           var user_event_event_no = connection.query(`SELECT MAX(event_no) FROM event`);
       //           var USERID = connection.query('SELECT USERID FROM user WHERE admin = 0');
