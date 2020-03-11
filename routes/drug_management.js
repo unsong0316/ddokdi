a04c4475969d9515f3e251c1196b8e1b7019fde8
unsong0316 <uschoe1994@gmail.com>
2020-03-11 16:20:19 +0900

    dd

--- routes/drug_management.js
+++ routes/drug_management.js
@@ -1,7 +1,7 @@
 var mysql      = require('mysql');
 var connection = mysql.createConnection({
   host     : 'localhost',
-  user     : 'roots',
+  user     : 'root',
   password : 'song',
   database : 'ddokdi_db'
 });
